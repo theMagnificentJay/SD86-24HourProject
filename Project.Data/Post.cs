76cfeed636f85e6f269e3795fe2b8654a83c38ab
theMagnificentJay <reedjoshua408@gmail.com>
2021-06-01 14:58:42 -0500

    changed post.cs

--- Project.Data/Post.cs
+++ Project.Data/Post.cs
@@ -12,11 +12,11 @@ namespace Project.Data
         [Key]
         public int PostId { get; set; }
         [Required]
-        public Guid OwnerId { get; set; }
-        [Required]
         public string Title { get; set; }
         [Required]
-        public string Content { get; set; }
+        public string Text { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
 
         [Required]
         public DateTimeOffset Created { get; set; }
