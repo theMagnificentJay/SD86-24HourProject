5246671cbaa7d4fc4b3b21e2321b8b49243f3f95
theMagnificentJay <reedjoshua408@gmail.com>
2021-06-01 14:23:05 -0500

    added post.cs and updated IdenityModels.cs to include Post.cs

---
+++ Project.Data/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Modified { get; set; }
+    }
+}
