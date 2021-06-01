07900f62b44d3b8965f42e6f3dd36ec303bb4345
Dustin Davis <dustindavis.d2@gmail.com>
2021-06-01 16:49:19 -0400

    Modified Post.cs; added comment.cs and Reply.cs

--- Project.Data/Comment.cs
+++ Project.Data/Comment.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project.Data
 {
-    public class Post
+    public class Comment
     {
         [Key]
-        public int PostId { get; set; }
-        [Required]
-        public string Title { get; set; }
+        [ForeignKey(nameof(Post))]
+        public int Id { get; set; }
+        public virtual Post Post { get; set; }
         [Required]
         public string Text { get; set; }
-        public virtual Comment Comment { get; set; }
         [Required]
         public Guid AuthorId { get; set; }
 
--- Project.Data/Reply.cs
+++ Project.Data/Reply.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project.Data
 {
-    public class Post
+    public class Reply
     {
         [Key]
-        public int PostId { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Text { get; set; }
+        [ForeignKey(nameof(Comment))]
+        public int Id { get; set; }
         public virtual Comment Comment { get; set; }
         [Required]
-        public Guid AuthorId { get; set; }
-
+        public string Text { get; set; }
         [Required]
-        public DateTimeOffset Created { get; set; }
-        public DateTimeOffset? Modified { get; set; }
+        public Guid AuthorId { get; set; }
     }
 }
